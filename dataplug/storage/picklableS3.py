01fda9ffebf8ef286270c8a8c840106cf7460c18
Kamilbur <kamil.burkiewicz@gmail.com>
2025-03-24 15:17:19 +0100

    (fix) Compute checksum only when required
    In botocore>=1.36 there are some problems with writing to non-AWS s3 buckets. A solution is to set AWS_REQUEST_CHECKSUM_CALCULATION env variable to WHEN_REQUIRED. It should be changed as soon as botocore is compatible with other s3 interfaces.

--- dataplug/storage/picklableS3.py
+++ dataplug/storage/picklableS3.py
@@ -380,5 +380,5 @@ class S3Path(PurePath):
 
 if sys.version_info < (3, 12):
     S3Path._flavour = _S3Flavour()
-# else:
-#     os.environ['AWS_REQUEST_CHECKSUM_CALCULATION'] = 'WHEN_REQUIRED'
+else:
+    os.environ['AWS_REQUEST_CHECKSUM_CALCULATION'] = 'WHEN_REQUIRED'
